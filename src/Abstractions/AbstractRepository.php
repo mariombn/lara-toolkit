4eee68fb3941a5438520469b2290f0be62ca002f
Mario de Moraes Barros Neto <mariombn@gmail.com>
2024-11-29 15:38:57 -0300

    mais coisas

--- src/Abstractions/AbstractRepository.php
+++ src/Abstractions/AbstractRepository.php
@@ -9,11 +9,22 @@ abstract class AbstractRepository
 {
     protected string $modelClass = Model::class;
 
+    /**
+     * Retorna uma instância vazia do modelo.
+     *
+     * @return Model
+     */
     public function getEmptyModel(): Model
     {
         return new $this->modelClass;
     }
 
+    /**
+     * Retorna um modelo pelo ID.
+     *
+     * @param int $id
+     * @return Model|null
+     */
     public function getById(int $id): ?Model
     {
         $model = $this->modelClass::query()->where('id', $id)->first();
@@ -21,6 +32,12 @@ abstract class AbstractRepository
         return $model;
     }
 
+    /**
+     * Criar um novo modelo.
+     *
+     * @param array $data
+     * @return Model
+     */
     public function insert(array $data): Model
     {
         $model = $this->getEmptyModel();
@@ -30,6 +47,12 @@ abstract class AbstractRepository
         return $model;
     }
 
+    /**
+     * Salvar um modelo.
+     *
+     * @param Model $model
+     * @return Model
+     */
     public function save(Model $model): Model
     {
         $model->save();
@@ -37,6 +60,13 @@ abstract class AbstractRepository
         return $model;
     }
 
+    /**
+     * Atualizar um modelo.
+     *
+     * @param Model $model
+     * @param array $data
+     * @return Model
+     */
     public function update(Model $model, array $data): Model
     {
         $model->update($data);
@@ -44,9 +74,14 @@ abstract class AbstractRepository
         return $model;
     }
 
+    /**
+     * Remover uma Collection de Modelos
+     *
+     * @return Collection
+     */
     public function getAll(): Collection
     {
-        $model = app($this->modelClass);
+        $model = $this->getEmptyModel();
         return $model->all();
     }
 }
